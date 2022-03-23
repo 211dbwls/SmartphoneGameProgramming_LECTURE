15fe26c3aa9eb51ecaf3fa69a7fbf09aa9617efb
211dbwls <211dbwls@naver.com>
2022-03-24 00:39:16 +0900

    ImageSwitcher - 버튼 클릭시 이미지 변경

--- 220316_W_ImageSwitcher/app/src/main/java/com/example/imageswitcher/MainActivity.java
+++ 220316_W_ImageSwitcher/app/src/main/java/com/example/imageswitcher/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.imageswitcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    protected static final int[] IMG_TEXT = new int[] {
+            R.mipmap.pic1,
+            R.mipmap.pic2,
+            R.mipmap.pic3,
+            R.mipmap.pic4,
+            R.mipmap.pic5
+    };
+
+    protected int page;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +30,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtnPrev(View view) {
         Log.d(TAG, "Prev Button Press");
+        setPage(page - 1);
     }
 
     public void onBtnNext(View view) {
         Log.d(TAG, "Next Button Press");
+        setPage(page + 1);
+    }
+
+    private void setPage(int newPage) {
+        page = newPage;
+
+        ImageView iv =  findViewById(R.id.whaleImage);
+        iv.setImageResource(IMG_TEXT[page-1]);
     }
 }
