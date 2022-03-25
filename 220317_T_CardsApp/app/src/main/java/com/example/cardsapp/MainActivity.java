a4ab26fd354089b259a7f091cc13d6985dd6a381
211dbwls <211dbwls@naver.com>
2022-03-25 18:01:49 +0900

    CardsApp - 클릭한 버튼 확인

--- 220317_T_CardsApp/app/src/main/java/com/example/cardsapp/MainActivity.java
+++ 220317_T_CardsApp/app/src/main/java/com/example/cardsapp/MainActivity.java
@@ -3,17 +3,44 @@ package com.example.cardsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int[] BUTTON_IDS = new int[] {  // 버튼 id
+            R.id.card_00, R.id.card_01, R.id.card_02,R.id.card_03,
+            R.id.card_10, R.id.card_11, R.id.card_12,R.id.card_13,
+            R.id.card_20, R.id.card_21, R.id.card_22,R.id.card_23,
+            R.id.card_30, R.id.card_31, R.id.card_32,R.id.card_33
+    };
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void onBtnCard(View view) {
+    public void onBtnRestart(View view) {
+        Log.d(TAG, "Restart");
+    }
 
+    public void onBtnCard(View view) {
+        ImageButton imageButton = (ImageButton) view;  // 클릭한 ImageButton 가져옴
+
+        int cardIndex = findButtonIndex(imageButton.getId());  // 카드 인덱스 구함
+        Log.d(TAG, "Card: " + cardIndex);
+    }
+
+    private int findButtonIndex(int id) {  // 카드 인덱스 구하는 함수
+        for(int i = 0; i < BUTTON_IDS.length; ++i) {
+            if(id == BUTTON_IDS[i]) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
