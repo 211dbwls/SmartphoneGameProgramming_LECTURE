46dd13e5e18c0a43e962f5ac3d3037d987d793eb
211dbwls <211dbwls@naver.com>
2022-03-25 21:29:49 +0900

    MoreControls - TextWatcher 두번째 방법

--- 220323_W_MoreControls/app/src/main/java/com/example/morecontrols/MainActivity.java
+++ 220323_W_MoreControls/app/src/main/java/com/example/morecontrols/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher {  // TextWatcher 두번째 방법
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         outputTextView = findViewById(R.id.outputTextView);
 
         // TextWatcher 첫번째 방법
-        editText.addTextChangedListener(new TextWatcher() {
+        /*editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {  // 입력하기 전에 호출
                 Log.v(TAG, "before");
@@ -45,7 +45,26 @@ public class MainActivity extends AppCompatActivity {
             public void afterTextChanged(Editable editable) {  // 입력 끝난 후 호출
                 Log.v(TAG, "after");
             }
-        });
+        });*/
+
+        editText.addTextChangedListener(this);  // TextWatcher 두번째 방법
+    }
+
+    // TextWatcher 두번째 방법
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {  // 입력하기 전에 호출
+        Log.v(TAG, "before");
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {  // 변화가 있을 때 호출
+        Log.d(TAG, "text change: " + charSequence);
+        outputTextView.setText("Text Length: " + charSequence.length());
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {  // 입력 끝난 후 호출
+        Log.v(TAG, "after");
     }
 
     public void onCheckBox(View view) {
