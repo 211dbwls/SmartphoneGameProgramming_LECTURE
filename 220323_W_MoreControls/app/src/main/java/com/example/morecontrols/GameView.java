5d5369ca543ea801a74565eed9f158b81e7ca4ec
211dbwls <211dbwls@naver.com>
2022-03-25 21:35:45 +0900

    MoreControls - GameView 생성

---
+++ 220323_W_MoreControls/app/src/main/java/com/example/morecontrols/GameView.java
@@ -0,0 +1,11 @@
+package com.example.morecontrols;
+
+import android.content.Context;
+import android.view.View;
+
+public class GameView extends View {
+
+    public GameView(Context context) {  // 생성자
+        super(context);
+    }
+}
