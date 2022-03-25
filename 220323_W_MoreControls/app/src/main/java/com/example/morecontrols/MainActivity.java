3f85bcdf3c51614083e1c15023e42ab3870aa7a4
211dbwls <211dbwls@naver.com>
2022-03-25 20:18:14 +0900

    MoreControls - findViewById 한번만 실행되도록 코드 수정

--- 220323_W_MoreControls/app/src/main/java/com/example/morecontrols/MainActivity.java
+++ 220323_W_MoreControls/app/src/main/java/com/example/morecontrols/MainActivity.java
@@ -13,25 +13,29 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    CheckBox checkBox;
+    EditText editText;
+    TextView outputTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkBox = findViewById(R.id.checkbox);
+        editText = findViewById(R.id.nameEdit);
+        outputTextView = findViewById(R.id.outputTextView);
     }
 
     public void onCheckBox(View view) {
         CheckBox cb = (CheckBox) view; 
-        Log.d(TAG, "Checked: " + cb.isChecked());
+        Log.d(TAG, "Checked: " + cb.isChecked());  // CheckBox 체크상태 확인
     }
 
     public void onBtnDoIt(View view) {
-        CheckBox cb = findViewById(R.id.checkbox);
-        Log.d(TAG, "onBtnDoIt(), Checked: " + cb.isChecked());
+        Log.d(TAG, "onBtnDoIt(), Checked: " + checkBox.isChecked());  // CheckBox 체크상태 확인
 
-        EditText et = findViewById(R.id.nameEdit);
-        String text = et.getText().toString();
-
-        TextView tv = findViewById(R.id.outputTextView);
-        tv.setText(text);
+        String text = editText.getText().toString();  // EditText 텍스트 가져옴
+        outputTextView.setText(text);  // TextView 텍스트를 EditText 텍스트로 변경
     }
 }
