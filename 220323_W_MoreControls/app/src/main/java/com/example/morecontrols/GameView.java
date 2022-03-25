c7d7a2a2bc4d0e33fa166f266472ed290e0ca30f
211dbwls <211dbwls@naver.com>
2022-03-25 21:49:10 +0900

    MoreControls - GameView xml로 편집 가능하도록 생성자 구현

--- 220323_W_MoreControls/app/src/main/java/com/example/morecontrols/GameView.java
+++ 220323_W_MoreControls/app/src/main/java/com/example/morecontrols/GameView.java
@@ -1,11 +1,21 @@
 package com.example.morecontrols;
 
 import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class GameView extends View {
 
+    private static final String TAG = GameView.class.getSimpleName();
+
     public GameView(Context context) {  // 생성자
         super(context);
+        Log.d(TAG, "GameView cons");
+    }
+
+    public GameView(Context context, AttributeSet as) {  // xml로 편집 가능하도록 하는 생성자
+        super(context, as);
+        Log.d(TAG, "GameView cons with as");
     }
 }
