15512a6728791c9b931d6c57b5962ecd4eb7764f
211dbwls <211dbwls@naver.com>
2022-03-31 16:21:05 +0900

    220331 수업 - SampleGame - fps 구해서 텍스트 그리기

--- 220331_T_SampleGame/app/src/main/java/com/example/samplegame/GameView.java
+++ 220331_T_SampleGame/app/src/main/java/com/example/samplegame/GameView.java
@@ -5,6 +5,8 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Handler;
 import android.util.AttributeSet;
@@ -26,6 +28,11 @@ public class GameView extends View {  // View 상속받음.
 
     private int ballDx, ballDy;  // 축구공 이동 크기 나타내는 변수.
 
+    private long lastTimeMillis;  // 기억하는 시각.
+
+    private int framePerSecond;  // fps.
+    private Paint fpsPaint = new Paint();  // fps 그릴 때 사용할 Paint.
+
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
@@ -36,6 +43,11 @@ public class GameView extends View {  // View 상속받음.
     }
 
     private void updateGame() {  // View를 다시 그리는 함수.
+        long now = System.currentTimeMillis();  // 현재 시각.
+        int elapsed = (int) (now - lastTimeMillis);  // 전 프레임부터 현재 프레임까지 흐른 시간 구함.
+        framePerSecond = 1000/elapsed;  // fps 구함.
+        lastTimeMillis = now;  // 현재 시각 저장.
+
         update();  // 게임 내용 업데이트하는 함수.
 
         invalidate();  // 다시 그려지는 것을 예약하는 함수.
@@ -56,12 +68,20 @@ public class GameView extends View {  // View 상속받음.
 
         ballDx = 10;  // 축구공 이동하는 크기 초기값 설정.
         ballDy = 10;
+
+        fpsPaint.setColor(Color.BLUE);  // text 색 설정.
+        fpsPaint.setTextSize(50);  // text 크기 설정.
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         // super.onDraw(canvas);
+
         canvas.drawBitmap(soccerBitmap, srcRect, dstRect, null);
+
+        // canvas.drawText(String.valueOf(framePerSecond), 0, 100, fpsPaint);
+        canvas.drawText("" + framePerSecond, 0, 100, fpsPaint);  // fps text 그림
+
         Log.d(TAG, "onDraw()");
     }
 
