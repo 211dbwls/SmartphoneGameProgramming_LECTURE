9194d8ab508c28e4c9abd4903de4157f7205ce42
211dbwls <211dbwls@naver.com>
2022-03-31 15:34:22 +0900

    220331 수업 - SampleGame 프로젝트 생성 및 GameView 생성

---
+++ 220331_T_SampleGame/app/src/main/java/com/example/samplegame/GameView.java
@@ -0,0 +1,16 @@
+package com.example.samplegame;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+    }
+
+    public GameView(Context context, AttributeSet as) {
+        super(context, as);
+    }
+
+}
