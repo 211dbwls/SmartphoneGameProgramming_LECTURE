0ac6daf31b1cbddd3200696ae8446f22c8e8d1ca
211dbwls <211dbwls@naver.com>
2022-04-07 16:06:02 +0900

    220407 수업 - DragonFlight - 이전에 사용한 파일 복사

---
+++ 220407_T_DragonFlight/app/src/main/java/com/example/dragonflight/Metrics.java
@@ -0,0 +1,14 @@
+package com.example.dragonflight;
+
+import android.content.res.Resources;
+
+public class Metrics {
+    public static int width;
+    public static int height;
+
+    public static float size(int dimenResId) {
+        Resources res = GameView.view.getResources();
+        float size = res.getDimension(dimenResId);
+        return size;
+    }
+}
