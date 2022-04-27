037615642e779371004a5e09feae8eeaf63e11d3
211dbwls <211dbwls@naver.com>
2022-04-27 09:41:30 +0900

    220427 수업 - DragonFlight - Bullet angle 없애기

--- 220407_T_DragonFlight/app/src/main/java/com/example/dragonflight/game/Bullet.java
+++ 220407_T_DragonFlight/app/src/main/java/com/example/dragonflight/game/Bullet.java
@@ -13,23 +13,20 @@ import com.example.dragonflight.R;
 public class Bullet implements GameObject, BoxCollidable {
     protected float x, y;
     protected final float length;
-    protected final float dx, dy;
-    protected final float ex, ey;
+
+    protected final float speed;
+
     protected static Paint paint;
 
     protected static float laserWidth;
 
     protected RectF boundgingBox = new RectF();
 
-    public Bullet(float x, float y, float angle) {
+    public Bullet(float x, float y) {
         this.x = x;
         this.y = y;
         this.length = Metrics.size(R.dimen.laser_length);
-        float speed = Metrics.size(R.dimen.laser_speed);
-        this.dx = (float) (speed * Math.cos(angle));
-        this.dy = (float) (speed * Math.sin(angle));
-        this.ex = (float) (length * Math.cos(angle));
-        this.ey = (float) (length * Math.sin(angle));
+        this.speed = Metrics.size(R.dimen.laser_speed);
 
         if (paint == null) {
             paint = new Paint();
@@ -38,25 +35,25 @@ public class Bullet implements GameObject, BoxCollidable {
             paint.setStrokeWidth(laserWidth);
         }
     }
+
     @Override
     public void update() {
         MainGame game = MainGame.getInstance();
 
         float frameTime = game.frameTime;
-        x += dx * frameTime;
-        y += dy * frameTime;
+        y -= speed * frameTime;
 
         float hw = laserWidth / 2;
-        boundgingBox.set(x - hw, y, x + hw, y + ey);
+        boundgingBox.set(x - hw, y, x + hw, y + length);
 
-        if(y < 0) {
+        if (y < 0) {
             game.remove(this);
         }
     }
 
     @Override
     public void draw(Canvas canvas) {
-        canvas.drawLine(x, y, x + ex, y + ey, paint);
+        canvas.drawLine(x, y, x, y + length, paint);
     }
 
     @Override
