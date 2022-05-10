b32c8057101c7941ef9a04db26a0db67a4e355c5
211dbwls <211dbwls@naver.com>
2022-05-10 22:36:42 +0900

    220511 수업 전 - DragonFlight - Animator 추가

--- 220407_T_DragonFlight/app/src/main/java/com/example/dragonflight/app/MainActivity.java
+++ 220407_T_DragonFlight/app/src/main/java/com/example/dragonflight/app/MainActivity.java
@@ -1,21 +1,67 @@
 package com.example.dragonflight.app;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 
 import com.example.dragonflight.R;
 import com.example.dragonflight.framework.GameView;
 import com.example.dragonflight.game.MainGame;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private View trees;
+    private ValueAnimator animator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        trees = findViewById(R.id.trees);
+        createAnimator();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startAnimation();
+    }
+
+    @Override
+    protected void onPause() {
+        stopAnimation();
+        super.onPause();
+    }
+
+    private void createAnimator() {
+        animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setDuration(5000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float progress = (Float)valueAnimator.getAnimatedValue();
+                Log.d(TAG, "Progerss: " + progress);
+            }
+        });
+    }
+
+    protected void startAnimation() {
+        animator.start();
+    }
+
+    protected void stopAnimation() {
+        animator.end();
     }
 
     public void onBtnStart(View view) {
