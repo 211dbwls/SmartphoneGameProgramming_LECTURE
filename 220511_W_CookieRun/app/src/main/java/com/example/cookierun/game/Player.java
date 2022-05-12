fd1723b5399c3f700591c7085f835c0cf6e4d103
211dbwls <211dbwls@naver.com>
2022-05-12 15:37:19 +0900

    220512 수업 - CookieRun - Box Collision 적용

--- 220511_W_CookieRun/app/src/main/java/com/example/cookierun/game/Player.java
+++ 220511_W_CookieRun/app/src/main/java/com/example/cookierun/game/Player.java
@@ -1,11 +1,13 @@
 package com.example.cookierun.game;
 
 import android.graphics.Rect;
+import android.graphics.RectF;
 
 import com.example.cookierun.R;
+import com.example.cookierun.framework.BoxCollidable;
 import com.example.cookierun.framework.SheetSprite;
 
-public class Player extends SheetSprite {
+public class Player extends SheetSprite implements BoxCollidable {
 
     private static final float FRAMES_PER_SECOND = 8f;  // 1초에 8장
 
@@ -16,10 +18,15 @@ public class Player extends SheetSprite {
         this.y = y;
         setDstRect(w, h);
         srcRects = new Rect[] {
-                new Rect(77, 409, 197, 544),
-                new Rect(77 + 272, 409, 197 + 272, 544),
-                new Rect(77 + 2 * 272, 409, 197 + 2 * 272, 544),
-                new Rect(77 + 3 * 272, 409, 197 + 3 * 272, 544),
+                new Rect(72 + 0 * 272, 404, 72+140 + 0 * 272, 404+140),
+                new Rect(72 + 1 * 272, 404, 72+140 + 1 * 272, 404+140),
+                new Rect(72 + 2 * 272, 404, 72+140 + 2 * 272, 404+140),
+                new Rect(72 + 3 * 272, 404, 72+140 + 3 * 272, 404+140),
         };
+    }
+
+    @Override
+    public RectF getBoundingRect() {
+        return dstRect;
     }
 }
