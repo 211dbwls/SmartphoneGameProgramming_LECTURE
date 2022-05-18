4c1fa2039dbc8a50d7a613d9e30f71fcfb2cf59a
211dbwls <211dbwls@naver.com>
2022-05-18 10:00:16 +0900

    220518 수업 - CookieRun - Collision box 조절

--- 220511_W_CookieRun/app/src/main/java/com/example/cookierun/game/Obstacle.java
+++ 220511_W_CookieRun/app/src/main/java/com/example/cookierun/game/Obstacle.java
@@ -1,12 +1,15 @@
 package com.example.cookierun.game;
 
+import android.graphics.RectF;
+
 import com.example.cookierun.R;
 import com.example.cookierun.framework.game.RecycleBin;
 import com.example.cookierun.framework.res.BitmapPool;
 
 public class Obstacle extends MapSprite {
-    private Modifier modifier;
-    private long createdOn;
+    protected Modifier modifier;
+    protected long createdOn;
+    protected RectF collisionBox = new RectF();
 
     public static Obstacle get(int index, float unitLeft, float unitTop) {
         Obstacle obs = (Obstacle) RecycleBin.get(Obstacle.class);
@@ -31,6 +34,7 @@ public class Obstacle extends MapSprite {
         }
 
         public void update(Obstacle obstacle, float frameTime) {
+            obstacle.collisionBox.set(obstacle.dstRect);
         }
 
         public void init(Obstacle obstacle, float unitLeft, float unitTop) {
@@ -44,9 +48,12 @@ public class Obstacle extends MapSprite {
 
     private static class AnimModifier extends Modifier {
         private final int[] mipmapResIds;
-        public AnimModifier(float heightUnit, int[] mipmapResIds) {
+        private final float topTransparent;
+
+        public AnimModifier(float heightUnit, int[] mipmapResIds, float topTransparent) {
             super(heightUnit);
             this.mipmapResIds = mipmapResIds;
+            this.topTransparent = topTransparent;
         }
 
         @Override
@@ -58,15 +65,15 @@ public class Obstacle extends MapSprite {
 
         @Override
         public void update(Obstacle obstacle, float frameTime) {
-            float elapsed = (System.currentTimeMillis() - obstacle.createdOn) / 1000.f;
-            final float start = 2.0f;
-            if (elapsed > start) {
-                final float fps = 8.0f;
-                int index = Math.round((elapsed - start) * fps);
-                if (index < mipmapResIds.length) {
-                    obstacle.bitmap = BitmapPool.get(mipmapResIds[index]);
-                }
-            }
+            RectF rect = obstacle.dstRect;
+            float height = rect.height();
+            float trans = height * topTransparent;
+            obstacle.collisionBox.set(
+                    rect.left,
+                    rect.top + trans,
+                    rect.right,
+                    rect.bottom
+            );
         }
     }
 
@@ -83,14 +90,14 @@ public class Obstacle extends MapSprite {
                     R.mipmap.epn01_tm01_jp1up_02,
                     R.mipmap.epn01_tm01_jp1up_03,
                     R.mipmap.epn01_tm01_jp1up_04,
-            }),
+            }, 64/131f),
             new AnimModifier(222/87f, new int[] {
                     R.mipmap.epn01_tm01_jp2up_01,
                     R.mipmap.epn01_tm01_jp2up_02,
                     R.mipmap.epn01_tm01_jp2up_03,
                     R.mipmap.epn01_tm01_jp2up_04,
                     R.mipmap.epn01_tm01_jp2up_05,
-            }),
+            }, 68/222f),
             new MoveModifier(482/86f, R.mipmap.epn01_tm01_sda),
     };
 
@@ -103,5 +110,10 @@ public class Obstacle extends MapSprite {
     public void update(float frameTime) {
         super.update(frameTime);
         modifier.update(this, frameTime);
+    }
+
+    @Override
+    public RectF getBoundingRect() {
+        return collisionBox;
     }
 }
