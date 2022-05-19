40eefb55ed2456e53cd3577a29cb6a6e0b8f157f
211dbwls <211dbwls@naver.com>
2022-05-19 15:50:03 +0900

    220519 수업 - SmoothingPath - 사용하지 않는 멤버 및 함수 삭제

---
+++ 220519_T_SmoothingPath/app/src/main/java/com/example/smoothingpath/PathView.java
@@ -0,0 +1,58 @@
+package com.example.smoothingpath;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class PathView extends View {
+    private int mExampleColor = Color.RED;
+
+    public PathView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public PathView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public PathView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.PathView, defStyle, 0);
+
+        mExampleColor = a.getColor(R.styleable.PathView_exampleColor, mExampleColor);
+
+        a.recycle();
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int contentWidth = getWidth() - paddingLeft - paddingRight;
+        int contentHeight = getHeight() - paddingTop - paddingBottom;
+    }
+
+    public int getExampleColor() {
+        return mExampleColor;
+    }
+
+}
